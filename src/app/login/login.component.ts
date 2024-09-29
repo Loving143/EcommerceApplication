449dfd2f72c23daa58e6d002392f5e3b4a46bffc
Prateek Kumar <Prateek.kumar949@gmail.com>
2024-09-29 23:53:52 +0530

    In this I have added the Login component and the Login component UI is working fine not done any backend part

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private fb : FormBuilder,
+    private snackbar : MatSnackBar,
+    private authService : AuthService,
+    private router : Router
+  ){}
+  loginForm!:FormGroup
+  hidePassword = true;
+
 
   ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email : [null, [Validators.required]],
+      password : [null, [Validators.required]]
+    })
+  }
+
+  togglePasswordVisibility(){
+    this.hidePassword = !this.hidePassword
+  }
+
+  onSubmit():void {
+    const userName = this.loginForm.get('email')!.value;
+    const password = this.loginForm.get('password')!.value;
+
+    this.authService.login(userName, password).subscribe(
+      (res)=>{
+        this.snackbar.open('Login Success','OK', {duration:5000})
+      },
+      (error) =>{
+        this.snackbar.open('Bad Credentials', 'Error', {duration : 5000})
+      }
+    )
   }
 
 }
